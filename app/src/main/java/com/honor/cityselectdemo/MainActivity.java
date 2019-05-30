81f58f9d91cf45eefdc5abb6e78b6cf058992258
wangrui <1254407151@qq.com>
2019-05-30 18:29:21 +0800

    Initial commit

---
+++ app/src/main/java/com/honor/cityselectdemo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.honor.cityselectdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.honor.cityselectdemo.CitySelect.CityPickerActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, CityPickerActivity.class));
+            }
+        });
+    }
+}
